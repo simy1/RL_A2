525b256dfbd2a2692ee7379c8654a68082ff011c
aleksandarkraljevic <a.kraljevic.2000@hotmail.com>
2023-03-10 14:31:32 +0100

    initial setup

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import gymnasium as gym
 import numpy as np
+import pygame
+import tensorflow as tf
 
-print('hello world')
+env = gym.make('CartPole-v1', render_mode='human')
+observation,info = env.reset()
+for _ in range(1000):
+    env.render()
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
+
