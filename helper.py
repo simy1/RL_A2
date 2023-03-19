fb3c21417b8417f97d911d3df8495f2f98d0d06b
Constantinos Georgiou <kotsiosg40@gmail.com>
2023-03-19 14:07:08 +0100

    2 functions added
    2 functions for running the program from the command line (uncomment also the code in the main_ER to be ok)

--- helper.py
+++ helper.py
@@ -1,4 +1,35 @@
 import numpy as np
+import sys
+
+def ckeckCMD():
+    acceptedTokens = ['dqn.py','--experience_replay','--target_network']
+    if len(sys.argv)<1 or len(sys.argv)>3:
+        return 'error_length'
+    for term in sys.argv:
+        if term not in acceptedTokens:
+            return 'error_term'
+        
+    if len(sys.argv) == 1:
+        return 'baseline'
+    elif '--experience_replay' in sys.argv and '--target_network' not in sys.argv:
+        return 'ER'
+    elif '--target_network' in sys.argv and '--experience_replay' not in sys.argv:
+        return 'TN'
+    elif '--experience_replay' in sys.argv and '--target_network' in sys.argv:
+        return 'ER_TN'
+
+
+def printNotAcceptedCMD(error_msg):
+    if error_msg == 'error_length':
+        print('The command was not accepted due to the following reason: too many/few arguments')
+    if error_msg == 'error_term':
+        print('The command was not accepted due to the following reason: wrong argument(s) given')
+    print('Please follow one of the examples listed below:')
+    print('$ python dqn.py')
+    print('$ python dqn.py --experience_replay')
+    print('$ python dqn.py --target_network')
+    print('$ python dqn.py --experience_replay --target_network')
+
 
 def exponential_anneal(t,start,final,decay_constant):
     ''' Linear annealing scheduler
