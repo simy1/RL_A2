1b5cb4e2097fa5af60dcb3c3a237d138b5fdbfa2
Simone van der Schot <simonevdschot@xs4all.nl>
2023-03-19 20:27:50 +0100

    unimportant changes, ignore

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import numpy as np
 import pygame
 import tensorflow as tf
 import sys
+import matplotlib.pyplot as plt
 
 # TODO figure out
 learning_rate = 10**(-1)
 gamma = 1  # discount factor
-epsilon = 0.4
+epsilon = 0.25
 
 env = gym.make('CartPole-v1', render_mode='human')
 
@@ -21,8 +22,8 @@ def initialize_model(learning_rate=1e-3):
     """
     # TODO hyperparameter settings / how many nodes and layers do we need?
     model = tf.keras.models.Sequential([
-      tf.keras.layers.Dense(10, activation='relu', input_shape=env.observation_space.shape, kernel_initializer='random_uniform'),
-      # tf.keras.layers.Dense(20, activation='relu'),
+      tf.keras.layers.Dense(20, activation='relu', input_shape=env.observation_space.shape, kernel_initializer='random_uniform'),
+      tf.keras.layers.Dense(10, activation='relu'),
       tf.keras.layers.Dense(2, activation='linear', kernel_initializer='random_uniform')
     ])
 
@@ -44,19 +45,24 @@ current_episode_length = 0
 
 observation, info = env.reset()
 predicted_q_values = main.predict(observation.reshape((1, 4)))
-for episode in range(1500):
+for episode in range(2000):
+
+    if episode > 1000:
+        epsilon = 0.1  # annealing
+
     current_episode_length += 1
     env.render()
 
     # let the main model predict the Q values based on the observation of the environment state
     # these are Q(S_t)
     if np.random.random() < epsilon:
+        print('random')
         # exploration
         action = np.random.randint(0, 2)
     else:
         action = np.argmax(predicted_q_values)  # take action with highest associated Q value
-    # print(f'predicted Q values {predicted_q_values}')
-    # print(f'Chosen action: {action}')
+    print(f'predicted Q values {predicted_q_values}')
+    print(f'Chosen action: {action}')
 
     new_observation, reward, terminated, truncated, info = env.step(action)
 
@@ -72,13 +78,22 @@ for episode in range(1500):
     observation = new_observation
     predicted_q_values = new_predicted_q_values
 
-
-
     if terminated or truncated:
+        print('it fell')
+        print(f'reward = {reward}')
+        sys.exit()
         episode_lengths.append(current_episode_length)
         current_episode_length = 0
         observation, info = env.reset()
 
+
+
 env.close()
 
 print('episode lengths: ', episode_lengths)
+
+plt.title('Episode lengths over time')
+plt.scatter(range(episode_lengths), episode_lengths)
+plt.xlabel('episode')
+plt.ylabel('length')
+plt.show()
