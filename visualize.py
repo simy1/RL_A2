efaae4f40947465bcf0cbcd8b46aec7173f722b1
Constantinos Georgiou <kotsiosg40@gmail.com>
2023-03-27 01:14:12 +0200

    methods for vizualization added

--- visualize.py
+++ visualize.py
@@ -1,5 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
+import pickle
+
+class LearningCurvePlot:
+
+    def __init__(self,title=None):
+        self.fig,self.ax = plt.subplots()
+        self.ax.set_xlabel('Time')
+        self.ax.set_ylabel('Reward')      
+        if title is not None:
+            self.ax.set_title(title)
+        
+    def add_curve(self,y,label=None):
+        ''' y: vector of average reward results
+        label: string to appear as label in plot legend '''
+        if label is not None:
+            self.ax.plot(y,label=label)
+        else:
+            self.ax.plot(y)
+    
+    def set_ylim(self,lower,upper):
+        self.ax.set_ylim([lower,upper])
+
+    def add_hline(self,height,label):
+        self.ax.axhline(height,ls='--',c='k',label=label)
+
+    def save(self,name='test.png'):
+        ''' name: string for filename of saved figure '''
+        self.ax.legend()
+        self.fig.savefig(name,dpi=300)
 
 def average_episode_length(episode_lengths):
     return np.cumsum(episode_lengths) / np.arange(1, len(episode_lengths)+1)
@@ -19,3 +49,32 @@ def plot_episode_length(episode_lengths, experiment_label):
     plt.tight_layout()
     plt.savefig(f'fig {experiment_label} 2')
     # plt.show()
+
+def smooth(y, window, poly=1):
+    '''
+    y: vector to be smoothed 
+    window: size of the smoothing window '''
+    return savgol_filter(y,window,poly)
+
+if __name__ == '__main__':
+    print('vizualizing results (using the code given in the assignment 1)')
+    smoothing_window = 5
+
+    for experiment_num in range(1,6+1):
+        Plot = LearningCurvePlot() 
+        for dqn_version in ['DQN','DQN-TN','DQN-ER','DQN-TN-ER']:
+            # learning curve: actual data
+            name = 'C:/Users/User/Documents/GitHub/RL_A2/RL-as2-details4runs/' + dqn_version + '/combination_' + str(experiment_num) + '.pkl'
+            a_file = open(name, "rb")
+            x_dict = pickle.load(a_file)
+            # print('keys:',x_dict.keys())
+            # print('values:',x_dict[list(x_dict.keys())[0]])
+            title = x_dict.keys()
+            Plot.ax.set_title(str(title),fontsize = 8)
+            learning_curve = x_dict[list(x_dict.keys())[0]]
+            learning_curve = smooth(learning_curve,smoothing_window)
+            Plot.add_curve(learning_curve,label=r'{}'.format(dqn_version))
+            a_file.close()
+
+        filename = 'test' + str(experiment_num) +  '_smooth_win' + str(smoothing_window) + '.png'
+        Plot.save(filename)
