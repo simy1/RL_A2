9d6053771681c1d664a0976c32cdd40fde2ccc63
Constantinos Georgiou <kotsiosg40@gmail.com>
2023-03-27 01:14:24 +0200

    script done

---
+++ shell_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# pyenv shell 3.8.0
+for i in {1..6}
+do
+    echo ">>> Shell script here <<<"
+    echo ">>> starting a new experiment: $i <<<"
+    /mnt/c/Users/User/.pyenv/pyenv-win/versions/3.8.0/python.exe c:/Users/User/Documents/GitHub/RL_A2/hyperparam_tune.py $i
+    echo ">>> completed the experiment: $i <<<"
+done
